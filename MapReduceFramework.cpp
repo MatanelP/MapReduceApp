ccfceb99577e97095d1436bf3d0a3a2ab42540ff
MatanelP <75391764+MatanelP@users.noreply.github.com>
2022-04-29 20:03:48 +0300

    starting implementation of the mapping

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,32 +1,127 @@
 //
 // Created by Matanel on 27/04/2022.
 //
+#include <cstdio>
+#include <cstdlib>
+#include <atomic>
+#include <algorithm>
+#include <iostream>
 # include "MapReduceFramework.h"
+#include "Barrier/Barrier.h"
 
+struct ThreadContext {
+  int threadID;
+  stage_t stage;
+  Barrier *barrier;
+  const MapReduceClient *client;
+  const InputVec *inputVec;
+  OutputVec *outputVec;
+  int totalThreadsCount;
+  IntermediateVec **intermediateVectors;
+  std::atomic<int> *mappingCounter;
 
-void emit2 (K2* key, V2* value, void* context){
+};
+
+struct Job {
+
+};
+
+void emit2 (K2 *key, V2 *value, void *context)
+{
+  ThreadContext *tc = (ThreadContext *) context;
+  tc->intermediateVectors[tc->threadID]->push_back ({key, value});
+}
+void emit3 (K3 *key, V3 *value, void *context)
+{
 
 }
-void emit3 (K3* key, V3* value, void* context){
 
+/**
+ * function to be called upon by each thread, starting its map-reduce
+ * algorithm part:
+ * map -> sort -> |barrier| -> shuffle (for tid 0) -> reduce
+ */
+void *threadMapReduce (void *arg)
+{
+  ThreadContext *tc = (ThreadContext *) arg;
+  //  mapping:
+  tc->stage = MAP_STAGE;
+  int index = (*(tc->mappingCounter))++;
+  while (index < tc->inputVec->size ())
+    {
+      InputPair inputPair = tc->inputVec->at (index);
+      tc->client->map (inputPair.first, inputPair.second, tc);
+      index = (*(tc->mappingCounter))++;
+    }
+  // sorting:
+  IntermediateVec *interVec = tc->intermediateVectors[tc->threadID];
+  std::sort (interVec->begin (), interVec->end (),
+             [] (IntermediatePair a, IntermediatePair b)
+             {
+               return *b.first < *a.first;
+             });
+
+  tc->barrier->barrier ();
+
+  return 0;
 }
 
-JobHandle startMapReduceJob(const MapReduceClient& client,
-                            const InputVec& inputVec, OutputVec& outputVec,
-                            int multiThreadLevel){
-  // tranforms inputVector to K2 and V2 vector for emit2
-  // creating threads
+JobHandle startMapReduceJob (const MapReduceClient &client,
+                             const InputVec &inputVec, OutputVec &outputVec,
+                             int multiThreadLevel)
+{
+  pthread_t threads[multiThreadLevel];
+  ThreadContext contexts[multiThreadLevel];
+  auto *barrier = new Barrier (multiThreadLevel);
+  auto **intermediateVectors = new IntermediateVec *[multiThreadLevel];
+  std::atomic<int> mappingCounter (0);
+
+
+//    creating new intermediate vectors
+  for (int i = 0; i < multiThreadLevel; ++i)
+    {
+      intermediateVectors[i] = new IntermediateVec ();
+    }
+
+//    init all contexts
+  for (int i = 0; i < multiThreadLevel; ++i)
+    {
+      ThreadContext &context = contexts[i];
+      context.threadID = i;
+      context.stage = UNDEFINED_STAGE;
+      context.barrier = barrier;
+      context.client = &client;
+      context.inputVec = &inputVec;
+      context.outputVec = &outputVec;
+      context.totalThreadsCount = multiThreadLevel;
+      context.intermediateVectors = intermediateVectors;
+      context.mappingCounter = &mappingCounter;
+    }
+
+  for (int i = 0; i < multiThreadLevel; ++i)
+    {
+      if (pthread_create (threads + i, NULL, threadMapReduce, contexts + i)
+          != 0)
+        {
+//        todo - print error
+          exit (EXIT_FAILURE);
+        }
+    }
 
   // creating JobHandler
+  return (JobHandle) new Job;
 
 }
 
-void waitForJob(JobHandle job){
+void waitForJob (JobHandle job)
+{
 
 }
-void getJobState(JobHandle job, JobState* state){
+void getJobState (JobHandle job, JobState *state)
+{
   // filling the state given according tto the jobHandler given
 }
-void closeJobHandle(JobHandle job){
+void closeJobHandle (JobHandle job)
+{
   // finishing job
 }
--- SampleClient.cpp
+++ SampleClient.cpp
@@ -44,8 +44,8 @@ public:
 
 			KChar* k2 = new KChar(i);
 			VCount* v2 = new VCount(counts[i]);
-			usleep(150000);
-			emit2(k2, v2, context);
+//			usleep(150000);
+            emit2(k2, v2, context);
 		}
 	}
 
